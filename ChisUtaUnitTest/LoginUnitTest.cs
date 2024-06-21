45054683ba1208a205fd179385b7b7c3053f96b8
Axl <axelolmar@gmail.com>
2024-06-20 20:24:57 -0600

    Se añadio proyecto de pruebas unitarias, y se realizo la primer prueba unitaria para el servicio de registro

---
+++ ChisUtaUnitTest/LoginUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ChisUTABackend.Services;
+using ChisUTABackend.Models;
+
+namespace ChisUtaUnitTest
+{
+    [TestClass]
+    public class LoginUnitTest
+    {
+        [TestMethod]
+        public void T001_Register()
+        {
+            UserServices userServices = new UserServices();
+
+            Users newUser = new Users()
+            {
+                Name = "Axel",
+                Email = "emaildeprueba@gmail.com",
+                Password = "unacontraseña@",
+                ConfirmPassword = "unacontraseña@"
+            };
+
+            ChisUtaResponse response = userServices.RegisterUser(newUser);
+
+            Assert.AreEqual(true, response.Success, "Ok");
+        }
+    }
+}
